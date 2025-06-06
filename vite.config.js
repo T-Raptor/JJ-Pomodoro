608d890eec40d55098486ad145097451dd0299c6
Ares <ares.popal@hotmail.com>
2025-06-06 03:00:38 +0200

    Remove incorrect URL from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-https://workspace.google.com/marketplace/app/highlight_tool_for_google_docs/446226911068?flow_type=2import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/

c75e7da8c06fda17d5df55ad9db3b80c3c781181
Ares <ares.popal@hotmail.com>
2025-06-06 02:51:21 +0200

    Fix incorrect import statement in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+https://workspace.google.com/marketplace/app/highlight_tool_for_google_docs/446226911068?flow_type=2import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
